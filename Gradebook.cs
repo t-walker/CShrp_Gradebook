cacd40fb081670cd270a593cff7f9383396aab73
Tyler Walker <twalker@eecs.wsu.edu>
2015-06-07 01:36:56 -0700

    Initial Upload

---
+++ Gradebook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CShrp_Gradebook
+{
+    class Gradebook
+    {
+        public Gradebook()
+        {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade < 100)
+            {
+                grades.Add(grade);
+            }
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0f;
+
+            foreach(float grade in grades)
+            {
+                sum += grade;
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+            }
+            
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        public string Name; 
+
+        private List<float> grades;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShrp_Gradebook
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Gradebook g1 = new Gradebook();
+            Gradebook g2 = g1;
+
+            Console.WriteLine(g2.Name);
+            //Gradebook book = new Gradebook();
+            //book.AddGrade(91f);
+            //book.AddGrade(89.1f);
+            //book.AddGrade(75);
+
+            //GradeStatistics stats = book.ComputeStatistics();
+
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.LowestGrade);
+            //Console.WriteLine(stats.HighestGrade);
+        }
+    }
+
+}
